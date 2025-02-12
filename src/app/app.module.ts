7629b89eec43c7614cf9a9f4abed4c5546f6588e
Renato-napoli <renato.napoli75@gmail.com>
2025-02-12 19:57:42 -0300

    Estrutura dos componentes tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
