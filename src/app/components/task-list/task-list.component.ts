b8bb47f9ab76b802591cb279f61dd9d10f7c5c78
Renato-napoli <renato.napoli75@gmail.com>
2025-02-13 10:43:11 -0300

    TS e HTML da lista de tarefas

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { TaskService } from './../../services/task.service';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
+  }
 }
--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,5 +1,5 @@
 export class Task {
-  id?: number;
+  id!: number;
   titulo?: string;
   completo?: boolean;
 }
